bd96007f79f5b6c2ed0c73b703b29a49c0349a57
6znj <119874842+6znj@users.noreply.github.com>
2020-10-30 08:18:33 +0000

    Networking

--- DecisionTree/ID3.py
+++ DecisionTree/ID3.py
@@ -28,4 +28,94 @@ class DecisionTree:
         entropy = 0.0
         for key in labelCounts:
             prop = float(labelCounts[key]) / num
-            entro
+            entropy -= prop * np.log2(prop)
+        return entropy
+
+    def _splitDataset(self, X, y, index, value):
+        """
+        :param X:
+        :param y:
+        :param index:
+        :param value:
+        :return: 数据集中特征下标为index，特征值为value的子数据集
+        """
+
+        ret = []
+        featVec = X[:, index]
+        X = X[:, [i for i in range(X.shape[1]) if i != index]]
+        for i in range(len(featVec)):
+            if featVec[i] == value:
+                ret.append(i)
+        return X[ret, :], y[ret]
+
+    def _chooseBestFeatureToSplit(self, X, y):
+        """
+        :param X:
+        :param y:
+        :return: bestFeature的index
+        """
+        numFeatures = X.shape[1]
+        oldEntropy = self._calcEntropy(y)
+        bestInfoGain = 0.0
+        bestFeatureIndex = -1
+        # 对每个feature都计算一下信息增益，并用bestInfoGain记录最大的那个
+        for i in range(numFeatures):
+            featureList = X[:, i]
+            uniqueVals = set(featureList)
+            newEntropy = 0.0
+            # 计算每种划分方式的信息熵
+            for value in uniqueVals:
+                sub_X, sub_y = self._splitDataset(X, y, i, value)
+                prob = len(sub_y) / float(len(y))
+                newEntropy += prob * self._calcEntropy(sub_y)
+            # 计算信息增益
+            infoGain = oldEntropy - newEntropy
+            if infoGain > bestInfoGain:
+                bestInfoGain = infoGain
+                bestFeatureIndex = i
+        return bestFeatureIndex
+
+    def _majorityCnt(self, labelList):
+        """
+        :param labelList:
+        :return: labelList中出现次数最多的label
+        """
+
+        labelCount = {}
+        for vote in labelList:
+            if vote not in labelCount.keys():
+                labelCount[vote] = 0
+            labelCount[vote] += 1
+        sortedLabelCount = sorted(labelCount.items(), key=lambda x: x[1], reverse=True)
+        return sortedLabelCount[0][0]
+
+    def _createTree(self, X, y, featureIndex):
+        """
+        构建决策树
+        :param X:
+        :param y:
+        :param featureIndex: 元组，记录X中特征在原始数据中对应index
+        :return:
+        """
+        labelList = list(y)
+        # 所有类别都相同，返回该label
+        if labelList.count(labelList[0]) == len(labelList):
+            return labelList[0]
+        # 没有特征可分割，返回出现次数最多的label
+        if len(featureIndex) == 0:
+            return self._majorityCnt(labelList)
+
+        # 可以继续分割，找寻最佳分割点
+        bestFeatureIndex = self._chooseBestFeatureToSplit(X, y)
+        bestFeatStr = featureIndex[bestFeatureIndex]
+        featureIndex = list(featureIndex)
+        featureIndex.remove(bestFeatStr)
+        featureIndex = tuple(featureIndex)
+
+        # 用字典存储决策树
+        myTree = {bestFeatStr: {}}
+        featValues = X[:, bestFeatureIndex]
+        uniqueVals = set(featValues)
+        for value in uniqueVals:
+            # 对每个value递归建树
+            sub_X, sub_
