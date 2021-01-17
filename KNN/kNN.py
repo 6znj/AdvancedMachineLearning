e7906ee35f34a05fc37be1b23f96d80e6b9fc749
6znj <119874842+6znj@users.noreply.github.com>
2021-01-17 11:17:23 +0000

    Added missing semicolon

--- KNN/kNN.py
+++ KNN/kNN.py
@@ -38,4 +38,51 @@ def preprocessing():
 
     # 测试集
     testFileList = listdir("testData/")
-    m_test = len(test
+    m_test = len(testFileList)
+    testData = np.zeros((m_test, 1024))
+    testLabel = []
+    for i in range(m_test):
+        testData[i, :] = img2vector("testData/" + testFileList[i])
+        testLabel.append(int(testFileList[i].split('_')[0]))
+    testLabel = np.array(testLabel)
+    return trainData, trainLabel, testData, testLabel
+
+
+# kNN算法
+def kNN(testData, trainDataMat, trainLabelMat, k):
+    m = trainDataMat.shape[0]
+
+    # 计算距离,使用欧式距离
+    difMat = trainDataMat - testData
+    sqDifMat = difMat ** 2
+    sqDistance = sqDifMat.sum(axis=1)
+    distance = sqDistance ** 0.5
+
+    # 对距离向量进行排序，获得其排序后的index
+    sortedDistanceIndicies = distance.argsort()
+
+    # 对前k个label进行计数排序
+    classCount = {}
+    for i in range(k):
+        voteLabel = trainLabelMat[sortedDistanceIndicies[i]]
+        classCount[voteLabel] = classCount.get(voteLabel, 0) + 1
+
+    # 按item进行排序逆序排序，第一个label即为选举出的label
+    result = sorted(classCount.items(), key=lambda item: item[1], reverse=True)
+    return result[0][0]
+
+
+if __name__ == "__main__":
+    # 获取训练集和测试集的向量
+    trainDataMat, trainLabelMat, testDataMat, testLabelMat = preprocessing()
+
+    m = testDataMat.shape[0]
+    count = 0
+    # 预测过程
+    for i in range(m):
+        predict = kNN(testDataMat[i, :], trainDataMat, trainLabelMat, 5)
+        if predict != testLabelMat[i]:
+            count += 1
+        print("预测结果为: %d, 期望结果为: %d" % (predict, testLabelMat[i]))
+
+    print("预测错误率为: %.2f %%" % (count / m * 100))
